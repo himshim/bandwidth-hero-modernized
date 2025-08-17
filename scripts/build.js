7ceab45e10095ecc10d4d2c715006c8bbc975368
himshim26 <97538806+himshim@users.noreply.github.com>
2025-08-17 11:59:05 +0530

    Update build.js

--- scripts/build.js
+++ scripts/build.js
@@ -8,8 +8,19 @@ async function build() {
   await fs.remove('dist');
   await fs.ensureDir('dist');
 
-  // Copy source files to dist
-  await fs.copy('src', 'dist');
+  // Copy source files to dist (excluding _locales)
+  const items = await fs.readdir('src', { withFileTypes: true });
+  for (const item of items) {
+    if (item.name !== '_locales') {
+      const srcPath = path.join('src', item.name);
+      const destPath = path.join('dist', item.name);
+      if (item.isDirectory()) {
+        await fs.copy(srcPath, destPath);
+      } else {
+        await fs.copy(srcPath, destPath);
+      }
+    }
+  }
 
   // Update version in manifest
   await replace({
