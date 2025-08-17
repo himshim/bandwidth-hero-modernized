2ff3dd895925dd6e4d12d65c46b64fe42da1f565
himshim26 <97538806+himshim@users.noreply.github.com>
2025-08-17 09:18:40 +0530

    Add basic background script for MV3 Bandwidth Hero Modernized

---
+++ src/background.js
@@ -0,0 +1,7 @@
+// Bandwidth Hero Modernized background script for MV3
+
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Bandwidth Hero Modernized installed!");
+});
+
+// You would add proxy logic, compression triggers, and other functionality here.
